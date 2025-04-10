da44d1cc538c43b2691c3d472c46285558d3bd6c
Krish Shah <ks964982@gmail.com>
2025-04-10 15:32:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/getUsers", async (req, res) => {
     await client.connect(URL);
     console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
+    const db = client.db("users-db");
     const data = await db.collection('users').find({}).toArray();
     
     client.close();
